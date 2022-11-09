91a938825da0610c503393ae7e565fb9436abaf1
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2022-11-09 16:16:25 +0000

    fixed

--- 2/ShootingGame/src/Main.java
+++ 2/ShootingGame/src/Main.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Main{
     public static void main(String args[]){
         String [][] gameMap = {
-                {"0","1","2","3","4","5"},
-                {"1","-","-","-","-","-"},
-                {"2","-","-","-","-","-"},
-                {"3","-","-","-","-","-"},
-                {"4","-","-","-","-","-"},
-                {"5","-","-","-","-","-"},
+                {"-","-","-","-","-"},
+                {"-","-","-","-","-"},
+                {"-","-","-","-","-"},
+                {"-","-","-","-","-"},
+                {"-","-","-","-","-"},
         };
         int [] randomGoal = {(int)(Math.ceil(Math.random()*gameMap.length)), (int)(Math.ceil(Math.random()*gameMap.length))};
 
@@ -18,59 +17,57 @@ class Main{
         boolean isGame = true;
 
         while(isGame){
-            System.out.println("Let's try to find the target. Enter vertical coordinate (from 1 to 5)");
+            System.out.println("Let's try to find the target. Enter X coordinate (from 1 to 5)");
+            int x = 0,y=0;
+            while(!scanner.hasNextInt() ){
+                System.out.println("Oops, it seems that you've entered not a number, please, try again!");
+                scanner.next();
+            }
 
             if(scanner.hasNextInt()){
-                int x = scanner.nextInt();
-                if(x>5 || x<1){
-                    System.out.println("Incorrect diapason");
-                    x = scanner.nextInt();
+                x = scanner.nextInt()-1;
+                while(x<0 || x>5 ){
+                    System.out.println("Incorrect diapason!");
+                    System.out.println("Let's try again. Enter X coordinate (from 1 to 5)");
+                    x = scanner.nextInt()-1;
                 }
 
-                System.out.println("Very good. Now enter the horizontal coordinate (from 1 to 5)");
-                int y;
-                if(!scanner.hasNextInt()){
-                    System.out.println("Oops, it seems that you've entered incorrect number, please, try again!");
-                    scanner.next();
+                System.out.println("Very good. Now enter the Y coordinate (from 1 to 5 from UP to DOWN)");
+                    y = scanner.nextInt()-1;
+                while(y<0 || y>5 ){
+                    System.out.println("Incorrect diapason!");
+                    System.out.println("Let's try again. Enter the Y coordinate (from 1 to 5 from UP to DOWN)");
+                    y = scanner.nextInt()-1;
                 }
-                else{
-                    y = scanner.nextInt();
-                    if(y>5 || y<1){
-                        System.out.println("Incorrect diapason");
-                       y = scanner.nextInt();
-                    }
 
-                    if(y == randomGoal[0] && x == randomGoal[1]){
+                    if(x == randomGoal[1] && y == randomGoal[0]){
                         System.out.println("You have won!");
-                        gameMap[x][y] = "x";
+                        gameMap[y][x] = "x";
                         String []stringArr = Arrays.deepToString(gameMap).split("],", 6);
-
+                        System.out.println("  1, 2, 3, 4, 5");
+                        int counter = 1;
                         for(String str: stringArr){
-                            System.out.println(str);
+                            str = str.replace("[","");
+                            System.out.println(counter+":"+str);
+                            counter++;
                         }
                         isGame = false;
 
                     }
                     else{
-                        gameMap[x][y] = "*";
+                        gameMap[y][x] = "*";
                         String []stringArr = Arrays.deepToString(gameMap).split("],", 6);
-
+                        int counter = 1;
+                        System.out.println("  1, 2, 3, 4, 5");
                         for(String str: stringArr){
-                            System.out.println(str);
+                            String newStr = str.replace("[","").replace("]","");
+                            System.out.println(counter+":"+newStr);
+                            counter++;
+
                         }
                     }
-                }
+
             }
-
-
-
-
-
-
-
-
-
-
         };
     }
 }
